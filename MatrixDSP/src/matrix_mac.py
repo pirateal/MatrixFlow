0ebe51259a7cf7e52363da9b8de07892618a7437
pirateal <Alex_Bookallil@hotmail.com>
2025-02-24 13:17:57 +1100

    Update matrix_mac.py

--- MatrixDSP/src/matrix_mac.py
+++ MatrixDSP/src/matrix_mac.py
@@ -1,1 +1,16 @@
-# Multiply-Accumulate (MAC) unit implemented with GPU matrix logic
+# matrix_mac.py
+import cupy as cp
+
+def matrix_mac(input_matrix, weight_matrix, bias=None):
+    """
+    Perform a multiply-accumulate (MAC) operation on matrices.
+    
+    input_matrix: cp.array of shape (M, N)
+    weight_matrix: cp.array of shape (N, P)
+    bias: cp.array of shape (P,), optional
+    Returns: cp.array of shape (M, P)
+    """
+    result = cp.dot(input_matrix, weight_matrix)
+    if bias is not None:
+        result += bias
+    return result
