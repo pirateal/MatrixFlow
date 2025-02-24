d5712a6c8969cdad1f58cfabeea427913cae4657
pirateal <Alex_Bookallil@hotmail.com>
2025-02-24 13:24:17 +1100

    Update test_fft_analysis.py

--- MatrixDSP/examples/test_fft_analysis.py
+++ MatrixDSP/examples/test_fft_analysis.py
@@ -1,1 +1,23 @@
-# Example script to analyze a signal using FFT
+# examples/test_fft_analysis.py
+import cupy as cp
+import numpy as np
+import matplotlib.pyplot as plt
+from src.fft_unit import FFTUnit
+
+def main():
+    fs = 44100
+    fft_unit = FFTUnit(fs)
+    t = cp.linspace(0, 1, fs)
+    signal = cp.sin(2 * cp.pi * 440 * t)
+    spectrum = fft_unit.fft_transform(signal)
+    freqs = cp.fft.fftfreq(signal.shape[0], 1/fs)
+    plt.figure()
+    plt.plot(cp.asnumpy(freqs), cp.asnumpy(cp.abs(spectrum)))
+    plt.title("FFT Spectrum")
+    plt.xlabel("Frequency (Hz)")
+    plt.ylabel("Magnitude")
+    plt.xlim(0, fs/2)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
